25da4defc784eeb31bec218e8226ef69e00f7c1d
imShourya18 <shouryasrivastava456@gmail.com>
2026-05-13 22:48:33 +0530

    Created the base structure for asgi requests.

---
+++ project/app/consumers.py
@@ -0,0 +1,11 @@
+from channels.generic.websocket import WebsocketConsumer
+
+class TestConsumer(WebsocketConsumer):
+    def connect(self):
+        pass
+
+    def receive(self, text_data = None, bytes_data = None):
+        pass
+
+    def disconnect(self, code):
+        pass
---
+++ project/project/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from django.urls import path
+from app.consumers import TestConsumer
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+application = get_asgi_application()
+
+ws_patterns = [
+    path('test/', TestConsumer.as_asgi, name='test'),
+]
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter(ws_patterns)
+})
